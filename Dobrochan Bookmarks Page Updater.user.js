120dce49c8815fd31caa46c2ad4415ce0d64a217
Jack Duck <bodyofchrist@cock.li>
2015-02-09 20:15:22 +0300

    tabs to 4 spaces

--- Dobrochan Bookmarks Page Updater.user.js
+++ Dobrochan Bookmarks Page Updater.user.js
@@ -13,29 +13,29 @@ var d = document,
     to = 1000 * 60 * 10; // check every 10 min
 
 function favBlink () {
-	d.title = '*' + d.title;
-	$(d).one('focus', function () {
+    d.title = '*' + d.title;
+    $(d).one('focus', function () {
         d.title = d.title.substr(1);
     });
 }
 
 function updateContainer (resp) {
-	$('table.threadlist tbody').remove();
-	$('table.threadlist').append($('tbody', resp));
+    $('table.threadlist tbody').remove();
+    $('table.threadlist').append($('tbody', resp));
 }
 
 function main() {
-	$.get(location.href)
-	.done(function(resp) {
-		if ( $('.highlight b', resp).length && $('tbody', resp).html() != $('tbody').html() )
+    $.get(location.href)
+    .done(function(resp) {
+        if ( $('.highlight b', resp).length && $('tbody', resp).html() != $('tbody').html() )
             favBlink();
 
         updateContainer(resp);
-	})
-	.always(function() {
+    })
+    .always(function() {
         var rand = Math.floor(Math.random() * 60 * 1000);
-		setTimeout(main, to + rand);
-	});
+        setTimeout(main, to + rand);
+    });
 }
 
 setTimeout(main, to);
