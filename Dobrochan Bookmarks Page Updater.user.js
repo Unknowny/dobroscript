22e284eb8f50e9d591389f8a80e2ab1988140f45
Jack Duck <bodyofchrist@cock.li>
2015-02-09 20:12:23 +0300

    interval change, refactor

--- Dobrochan Bookmarks Page Updater.user.js
+++ Dobrochan Bookmarks Page Updater.user.js
@@ -9,31 +9,33 @@
 // @version     1.0.3
 // ==/UserScript==
 
-var d = document, to = 20000;
+var d = document,
+    to = 1000 * 60 * 10; // check every 10 min
 
-function favicBlink() {
-	d.title='*'+d.title;
-	$(d).one('focus', function(){d.title=d.title.substr(1);});
+function favBlink () {
+	d.title = '*' + d.title;
+	$(d).one('focus', function () {
+        d.title = d.title.substr(1);
+    });
 }
 
-function insertTable(resp) {
+function updateContainer (resp) {
 	$('table.threadlist tbody').remove();
 	$('table.threadlist').append($('tbody', resp));
 }
 
-function isAnyChanges(resp) {
-	return $('tbody', resp).html() != $('tbody').html() && $('.highlight b', resp).length;
-}
-
-function check() {
+function main() {
 	$.get(location.href)
-		.done(function(resp) {
-			if ( isAnyChanges(resp) ){ favicBlink(); insertTable(resp); }
-			else { insertTable(resp); }
-		})
-		.always(function() {
-			setTimeout(check, to);
-		});
+	.done(function(resp) {
+		if ( $('.highlight b', resp).length && $('tbody', resp).html() != $('tbody').html() )
+            favBlink();
+
+        updateContainer(resp);
+	})
+	.always(function() {
+        var rand = Math.floor(Math.random() * 60 * 1000);
+		setTimeout(main, to + rand);
+	});
 }
 
-setTimeout(check, to);
+setTimeout(main, to);
