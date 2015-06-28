357ed117b6fd4bb1677b346fc9a795ea104d6378
Jack Duck <bodyofchrist@cock.li>
2015-06-28 16:20:46 +0300

    feat: <b> unwrap

--- Dobrochan Bookmarks Page Updater.user.js
+++ Dobrochan Bookmarks Page Updater.user.js
@@ -6,13 +6,13 @@
 // @homepage    https://github.com/Unknowny/dobroscript
 // @updateURL   https://github.com/Unknowny/dobroscript/raw/master/Dobrochan Bookmarks Page Updater.user.js
 // @downloadURL https://github.com/Unknowny/dobroscript/raw/master/Dobrochan Bookmarks Page Updater.user.js
-// @version     1.0.3
+// @version     1.0.4
 // ==/UserScript==
 
 var d = document,
     to = 1000 * 60 * 10; // check every 10 min
 
-function favBlink () {
+function faviconBlink () {
     d.title = '*' + d.title;
     $(d).one('focus', function () {
         d.title = d.title.substr(1);
@@ -25,17 +25,27 @@ function updateContainer (resp) {
 }
 
 function main() {
+    console.log('Fetching bookmarks page ' + new Date().toGMTString());
     $.get(location.href)
     .done(function(resp) {
         if ( $('.highlight b', resp).length && $('tbody', resp).html() != $('tbody').html() )
-            favBlink();
+            faviconBlink();
 
         updateContainer(resp);
     })
     .always(function() {
-        var rand = Math.floor(Math.random() * 60 * 1000);
+        var rand = Math.floor(Math.random() * 60 * 1000); // ~1 min random
         setTimeout(main, to + rand);
     });
 }
 
+function threadLinkClick (e) {
+    // removes bold font after thread link has been clicked
+    if (e.which == 2) {
+        var b = $(e.target).parent().find('b')
+        b.replaceWith(b.html())
+    }
+}
+
+$(document.body).on('mouseup', 'tr.highlight td:first-child a:first-child', threadLinkClick)
 setTimeout(main, to);
