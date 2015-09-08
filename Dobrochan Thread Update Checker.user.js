d2aa365ede12dc8ca2396bbac50e28d5a52f9e53
Jack Duck <bodyofchrist@cock.li>
2015-09-08 13:02:01 +0300

    refactor

--- Dobrochan Thread Update Checker.user.js
+++ Dobrochan Thread Update Checker.user.js
@@ -9,44 +9,49 @@
 // @version     1.0.3
 // ==/UserScript==
 
-var d = document,
-    $notice = crNotice(),
-    board = location.pathname.match(/[a-z]+/)[0],
-    lastID = $('.post:last').attr('id').substr(5);
-
-function crNotice() {
-    var $div = $('<div></div>');
-    $div.addClass('reply');
-    $div.css({
-        'top':'25px',
-        'right':'25px',
-        'position':'fixed',
-        'cursor':'pointer',
-        'padding':'4px',
-        'box-shadow':'2px 2px rgba(0, 0, 0, 0.12)',
-        'border':'1px solid black',
-        'display':'none'
-    });
-    $div.on('click', function(){location.reload();});
-    Hanabira.LC_ru ? $div.text('Новые посты. Обновить страницу.') : $div.text('New posts. Reload page.');
-    return $div;
+function makePopup () {
+    var div = $('<div class="reply"></div>');
+    div.css({
+            'top':'25px',
+            'right':'25px',
+            'position':'fixed',
+            'cursor':'pointer',
+            'padding':'4px',
+            'box-shadow':'2px 2px rgba(0, 0, 0, 0.12)',
+            'border':'1px solid black',
+            'display':'none'
+        })
+        .on('click', function () {location.reload();})
+        .text(Hanabira.LC_ru ? 'Новые посты. Обновить страницу.' : 'New posts. Reload page.');
+    return div;
 }
 
-function favicBlink() {
-    d.title='*'+d.title;
-    $(d).one('focus', function(){d.title=d.title.substr(1);});
+function faviconBlink () {
+    d.title = '*' + d.title;
+    $(d).one('focus', function () {d.title = d.title.substr(1);});
 }
 
-function notify() {
-    $notice.fadeIn();
-    favicBlink()
+function notify () {
+    popup.fadeIn();
+    faviconBlink();
 }
 
-function check() {
-    $.get('/api/thread/'+board+'/'+location.pathname.match(/\d+/)[0]+'/new.json?last_post='+lastID, function(resp){
-        resp.posts ? notify() : setTimeout(check, 20000);
+function check () {
+    $.get('/api/thread/' + board + '/' + thread + '/new.json?last_post=' + lastID, function(resp) {
+        if (resp.posts) {
+            notify();
+        }
+        else
+            setTimeout(check, 20000);
     });
 }
 
-$(d.body).append($notice);
+// Main
+var d = document;
+var popup = makePopup();
+var board = location.pathname.match(/[a-z]+/)[0];
+var thread = location.pathname.match(/\d+/)[0];
+var lastID = $('.post:last').attr('id').substr(5);
+
+$('body').append(popup);
 setTimeout(check, 20000);
