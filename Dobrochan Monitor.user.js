f981c6e261a03f998617657d6d87fe3b81a3c5e5
Jack Duck <bodyofchrist@cock.li>
2015-11-24 02:26:50 +0300

    fix css conflict with MagicExtension

--- Dobrochan Monitor.user.js
+++ Dobrochan Monitor.user.js
@@ -37,7 +37,7 @@ var list_limit = 30;
 var default_settings = {boards: ['b', 'azu']};
 var existing_boards = 'b u rf dt vg r cr lor mu oe s w hr a ma sw hau azu tv cp gf bo di vn ve wh fur to bg wn slow mad d news'.split(' ');
 var diff_url = '/api/chan/stats/diff.json';
-var main_css_url = 'https://rawgit.com/Unknowny/dobroscript/master/resources/monitor.css?c';
+var main_css_url = 'https://rawgit.com/Unknowny/dobroscript/master/resources/monitor.css?d';
 // var main_css_url = 'http://127.0.0.1:8080/resources/monitor.css'
 
 // Shims, Helpers, Shortcuts ///////////////////
